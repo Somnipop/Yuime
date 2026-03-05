38ea514a8845dfc6ed83277e20902992d6bd7611
dumbassmisa <luccacsg@gmail.com>
2026-03-05 12:40:59 -0300

    idk idc :100:

--- examples/basic_interface/src/main.c
+++ examples/basic_interface/src/main.c
@@ -1,5 +1,21 @@
+#define YUIME_IMPLEMENTATION
 #include "yuime/yuime.h"
 
 int main() {
-	yuime_init();
+	yuime_element_button btn = {
+		.minSize = {0.0f, 0.0f},
+		.maxSize = {255.0f, 255.0f},
+	
+		.geometry = {
+				.pivot = {0.5f, 0.05f},
+				.position = {
+					.scale = {0.5f, 0.5f},
+					.offset = {0.0f, 0.0f}
+				},
+				.size = {
+					.scale = {0.5f, 0.5f},
+					.offset = {0.0f, 0.0f}
+				}
+		}
+	};
 }
