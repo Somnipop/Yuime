784fda2daaa30580c81744d61342ae9584b7ff17
dumbassmisa <luccacsg@gmail.com>
2026-03-05 12:42:17 -0300

    include everything in yuime.h

--- include/yuime/yuime.h
+++ include/yuime/yuime.h
@@ -2,4 +2,13 @@
 
 #include "API.h"
 
+#include "vector2.h"
+#include "dimension2.h"
+#include "geometry.h"
+
+#include "context.h"
+#include "element.h"
+
+#include "elements/button.h"
+
 YUIME_API void yuime_init();
