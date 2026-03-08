02836b44f3a50abdb01407a1d1cd0da2554c0234
dumbassmisa <luccacsg@gmail.com>
2026-03-08 15:37:27 -0300

    make elements optional when building project

--- include/yuime/yuime.h
+++ include/yuime/yuime.h
@@ -3,10 +3,16 @@
 #include "API.h"
 
 #include "vector2.h"
+#include "rect.h"
 #include "dimension2.h"
 #include "geometry.h"
 
 #include "context.h"
+#include "event.h"
 #include "element.h"
 
+#include "alloc.h"
+
+#ifdef YUIME_BUILD_ELEMENTS
 #include "elements/button.h"
+#endif
