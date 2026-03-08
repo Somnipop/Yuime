915aed69aa8ded36054e88a149f83376a09d245e
dumbassmisa <luccacsg@gmail.com>
2026-03-08 15:36:33 -0300

    yuime geometry having size being a pointer is good so that multiple elements can have the same size and update everything in one go

--- include/yuime/geometry.h
+++ include/yuime/geometry.h
@@ -5,8 +5,11 @@
 #include "vector2.h"
 #include "dimension2.h"
 
-typedef struct {
-	yuime_vector2 pivot; ///< origin of the geometry
+/**
+ * @brief Rectangle geometry.
+ */
+typedef struct yuime_geometry_s {
+	yuime_vector2 pivot; ///< Origin of the geometry.
 	yuime_dim2 position;
-	yuime_dim2 size;
+	yuime_dim2* size; ///< Pointer to size. Why? Because size could be shared with other geometries.
 } yuime_geometry;
