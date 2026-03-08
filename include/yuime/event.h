0f08feeb9678fc7fadb70af4d5bff6dc6a41adf3
dumbassmisa <luccacsg@gmail.com>
2026-03-08 15:36:02 -0300

    WIP event call system

--- include/yuime/event.h
+++ include/yuime/event.h
@@ -2,25 +2,17 @@
 
 #include <stdint.h>
 
-typedef uint8_t yuime_event_flag_t;
-typedef uint8_t yuime_mouse_button_t;
-typedef uint8_t yuime_mouse_button_t;
-typedef float yuime_wheel_direction_t;
-typedef uint32_t yuime_key_t;
+#include "API.h"
 
-// Mouse
-#define YUIME_EVENT_MOUSE_PRESS_FLAG ((yuime_event_flag_t)1)
-typedef void(*on_mouse_press_callback_t)(yuime_mouse_button_t button);
+struct yuime_context_s;
+struct yuime_element_s;
+typedef void(*yuime_event_callback_t)(struct yuime_context_s* ctx, struct yuime_element_s* element, void* data);
 
-#define YUIME_EVENT_MOUSE_RELEASE_FLAG ((yuime_event_flag_t)2)
-typedef void(*on_mouse_release_callback_t)(yuime_mouse_button_t button);
-
-#define YUIME_EVENT_MOUSE_WHEEL_MOVE_FLAG ((yuime_event_flag_t)4)
-typedef void(*on_mouse_wheel_move_callback_t)(yuime_wheel_direction_t dir);
-
-// Keyboard
-#define YUIME_EVENT_KEY_PRESS_FLAG ((yuime_event_flag_t)8)
-typedef void(*on_key_press_callback_t)(yuime_key_t key);
-
-#define YUIME_EVENT_KEY_RELEASE_FLAG ((yuime_event_flag_t)16)
-typedef void(*on_key_release_callback_t)(yuime_key_t key);
+/**
+ * @brief Passes data to all elements via element->event_callback().
+ * 
+ * @param ctx 
+ * @param data Data to be passed.
+ * @return YUIME_API 
+ */
+YUIME_API void yuime_context_send_event(struct yuime_context_s* ctx, void* data);
