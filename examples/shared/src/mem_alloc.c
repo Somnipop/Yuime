45d84f34fa0de232268c90e5612c67d99f94e3a0
dumbassmisa <luccacsg@gmail.com>
2026-03-20 13:23:45 -0300

    add memory stats for debug mode

--- examples/shared/src/mem_alloc.c
+++ examples/shared/src/mem_alloc.c
@@ -1,9 +1,20 @@
 #include "mem_alloc.h"
 
 #include <stdlib.h>
+
 #ifndef NDEBUG
 #include <stdio.h>
+
+static size_t allocations = 0;
+static size_t allocations_in_bytes = 0;
+
+static size_t reallocations_total = 0;
+static size_t reallocations_total_in_bytes = 0;
+
+static size_t allocations_total = 0;
+static size_t allocations_total_in_bytes = 0;
 #endif // !NDEBUG
+
 
 uint8_t mem_alloc(void** ptr, size_t size) {
 	*ptr = malloc(size);
@@ -11,6 +22,11 @@ uint8_t mem_alloc(void** ptr, size_t size) {
 	if (*ptr) {
 #ifndef NDEBUG
 		printf("Allocated %zu bytes\n", size);
+		allocations++;
+		allocations_in_bytes += size;
+
+		allocations_total++;
+		allocations_total_in_bytes += size;
 #endif // !NDEBUG
 		return 1;
 
@@ -26,10 +42,26 @@ uint8_t mem_realloc(void** ptr, size_t current_size, size_t size) {
 	void* new_ptr = realloc(*ptr, size);
 
 	if (new_ptr) {
-		*ptr = new_ptr;
 #ifndef NDEBUG
-		printf("Re-allocated from %zu to %zu bytes\n", current_size, size);
+		if (current_size == 0 || *ptr == NULL) {
+			printf("Allocated calling mem_realloc(), from %zu to %zu bytes\n", current_size, size);
+			allocations++;
+			allocations_total++;
+		} else {
+			printf("Re-allocated from %zu to %zu bytes\n", current_size, size);
+			reallocations_total++;
+		}
+
+		allocations_in_bytes -= current_size;
+		allocations_in_bytes += size;
+
+		allocations_total_in_bytes -= current_size;
+		allocations_total_in_bytes += size;
+
+		reallocations_total_in_bytes -= current_size;
+		reallocations_total_in_bytes += size;
 #endif // !NDEBUG
+		*ptr = new_ptr;
 		return 1;
 
 	} else {
@@ -43,6 +75,18 @@ uint8_t mem_realloc(void** ptr, size_t current_size, size_t size) {
 void mem_free(void* ptr, size_t size) {
 	free(ptr);
 #ifndef NDEBUG
-		printf("Freed %zu bytes\n", size);
+	printf("Freed %zu bytes\n", size);
+	allocations--;
+	allocations_in_bytes -= size;
 #endif // !NDEBUG
 }
+
+#ifndef NDEBUG
+void mem_print_stats() {
+	printf("--- HEAP STATUS ---\nAllocations total: %zu (%zu bytes)\nReallocations total: %zu (%zu bytes)\nCurrently allocated: %zu (%zu bytes)\n-------------------\n",
+		allocations_total, allocations_total_in_bytes,
+		reallocations_total, reallocations_total_in_bytes,
+		allocations, allocations_in_bytes
+	);
+}
+#endif // !NDEBUG
--- examples/shared/src/mem_alloc.h
+++ examples/shared/src/mem_alloc.h
@@ -5,3 +5,7 @@
 uint8_t mem_alloc(void** ptr, size_t size);
 uint8_t mem_realloc(void** ptr, size_t current_size, size_t size);
 void mem_free(void* ptr, size_t size);
+
+#ifndef NDEBUG
+void mem_print_stats();
+#endif // !NDEBUG
