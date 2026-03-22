f0ebde323f992ab0210aab2d30f462e1ef429198
dumbassmisa <luccacsg@gmail.com>
2026-03-22 14:50:37 -0300

    reorganized node members

--- include/yuime/node.h
+++ include/yuime/node.h
@@ -8,12 +8,12 @@
 #include "math/geometry.h"
 
 typedef struct yuime_node_s {
+	struct yuime_node_s *parent;
+	struct yuime_node_s *first_child;
+	struct yuime_nide_s *next;
+
 	yuime_dim2_t min_size;
 	yuime_dim2_t max_size;
 	yuime_geometry_t geometry;
 	yuime_rect_t rect;
-
-	struct yuime_node_s *parent;
-	struct yuime_node_s* first_child;
-	struct yuime_nide_s* next;
 } yuime_node_t;
