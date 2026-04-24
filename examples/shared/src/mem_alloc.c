361f596ac5ad2d61fb5d596c6d377bee8f2c4e2d
kamiicode <luccacsg@gmail.com>
2026-04-24 12:50:42 -0300

    improved memory allocation debug stats

--- examples/shared/src/mem_alloc.c
+++ examples/shared/src/mem_alloc.c
@@ -8,17 +8,29 @@
 static size_t allocations = 0;
 static size_t allocations_in_bytes = 0;
 
-static size_t allocations_nodes = 0;
-static size_t allocations_widgets = 0;
-
-static size_t reallocations_total = 0;
-static size_t reallocations_total_in_bytes = 0;
-
 static size_t allocations_total = 0;
 static size_t allocations_total_in_bytes = 0;
 
+
+static size_t reallocations_total = 0;
+
+
+static size_t allocations_nodes = 0;
+static size_t allocations_nodes_in_bytes = 0;
+
 static size_t allocations_nodes_total = 0;
+static size_t allocations_nodes_total_in_bytes = 0;
+
+static size_t reallocations_nodes_total = 0;
+
+
+static size_t allocations_widgets = 0;
+static size_t allocations_widgets_in_bytes = 0;
+
 static size_t allocations_widgets_total = 0;
+static size_t allocations_widgets_total_in_bytes = 0;
+
+static size_t reallocations_widgets_total = 0;
 #endif // !NDEBUG
 
 
@@ -42,11 +54,17 @@ uint8_t mem_alloc(yuime_alloc_context_t ctx, void** ptr, size_t size) {
 		case YUIME_ALLOC_CONTEXT_NODE:
 			allocations_nodes++;
 			allocations_nodes_total++;
+
+			allocations_nodes_in_bytes += size;
+			allocations_nodes_total_in_bytes += size;
 			break;
 
 		case YUIME_ALLOC_CONTEXT_WIDGET:
 			allocations_widgets++;
 			allocations_widgets_total++;
+
+			allocations_widgets_in_bytes += size;
+			allocations_widgets_total_in_bytes += size;
 			break;
 
 		default:
@@ -68,42 +86,50 @@ uint8_t mem_realloc(yuime_alloc_context_t ctx, void** ptr, size_t current_size, 
 	(void)ctx;
 #endif
 
+	if (current_size == 0 || *ptr == NULL) {
+#ifndef NDEBUG
+		printf("mem_realloc() is calling mem_alloc()\n");
+#endif
+		return mem_alloc(ctx, ptr, size);
+	}
+
 	void* new_ptr = realloc(*ptr, size);
 
 	if (new_ptr) {
 #ifndef NDEBUG
-		if (current_size == 0 || *ptr == NULL) {
-			printf("Allocated calling mem_realloc(), from %zu to %zu bytes\n", current_size, size);
-			allocations++;
-			allocations_total++;
+		printf("Re-allocated from %zu to %zu bytes\n", current_size, size);
+		reallocations_total++;
 
-			switch (ctx) {
-			case YUIME_ALLOC_CONTEXT_NODE:
-				allocations_nodes++;
-				allocations_nodes_total++;
-				break;
-
-			case YUIME_ALLOC_CONTEXT_WIDGET:
-				allocations_widgets++;
-				allocations_widgets_total++;
-				break;
-
-			default:
-				break;
-			}
-		} else {
-			printf("Re-allocated from %zu to %zu bytes\n", current_size, size);
-			reallocations_total++;
-		}
-
-		allocations_in_bytes -= current_size;
 		allocations_in_bytes += size;
+		allocations_in_bytes -= current_size;
 
-		allocations_total_in_bytes -= current_size;
 		allocations_total_in_bytes += size;
+		allocations_total_in_bytes -= current_size;
 
-		reallocations_total_in_bytes -= current_size;
-		reallocations_total_in_bytes += size;
+		switch (ctx) {
+		case YUIME_ALLOC_CONTEXT_NODE:
+			reallocations_nodes_total++;
+
+			allocations_nodes_in_bytes += size;
+			allocations_nodes_in_bytes -= current_size;
+
+			allocations_nodes_total_in_bytes += size;
+			allocations_nodes_total_in_bytes -= current_size;
+			break;
+
+		case YUIME_ALLOC_CONTEXT_WIDGET:
+			reallocations_widgets_total++;
+
+			allocations_widgets_in_bytes += size;
+			allocations_widgets_in_bytes -= current_size;
+
+			allocations_widgets_total_in_bytes += size;
+			allocations_widgets_total_in_bytes -= current_size;
+			break;
+
+		default:
+			break;
+		}
 #endif // !NDEBUG
 		*ptr = new_ptr;
 		return 1;
@@ -131,10 +157,12 @@ void mem_free(yuime_alloc_context_t ctx, void* ptr, size_t size) {
 	switch (ctx) {
 	case YUIME_ALLOC_CONTEXT_NODE:
 		allocations_nodes--;
+		allocations_nodes_in_bytes -= size;
 		break;
 
 	case YUIME_ALLOC_CONTEXT_WIDGET:
-		allocations_widgets--;	
+		allocations_widgets--;
+		allocations_widgets_in_bytes -= size;
 		break;
 	}
 #endif // !NDEBUG
@@ -142,12 +170,16 @@ void mem_free(yuime_alloc_context_t ctx, void* ptr, size_t size) {
 
 #ifndef NDEBUG
 void mem_print_stats() {
-	printf("--- HEAP STATUS ---\nAllocations total: %zu (%zu bytes)\n - Nodes: %zu\n - Widgets: %zu\nReallocations total: %zu (%zu bytes)\nCurrently allocated: %zu (%zu bytes)\n - Nodes: %zu\n - Widgets: %zu\n-------------------\n",
+	printf("--- HEAP STATUS ---\nAllocations total: %zu (%zu bytes)\n - Nodes: %zu (%zu bytes)\n - Widgets: %zu (%zu bytes)\nReallocations total: %zu\n - Nodes: %zu\n - Widgets: %zu\nCurrently allocated: %zu (%zu bytes)\n - Nodes: %zu (%zu bytes)\n - Widgets: %zu (%zu bytes)\n-------------------\n",
 		allocations_total, allocations_total_in_bytes,
-		allocations_nodes_total, allocations_widgets_total,
-		reallocations_total, reallocations_total_in_bytes,
+		allocations_nodes_total, allocations_nodes_total_in_bytes,
+		allocations_widgets_total, allocations_widgets_total_in_bytes,
+		reallocations_total,
+		reallocations_nodes_total,
+		reallocations_widgets_total,
 		allocations, allocations_in_bytes,
-		allocations_nodes, allocations_widgets
+		allocations_nodes, allocations_nodes_in_bytes,
+		allocations_widgets, allocations_widgets_in_bytes
 	);
 }
 #endif // !NDEBUG
