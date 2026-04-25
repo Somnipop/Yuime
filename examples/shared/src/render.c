a55061b825de8ba46698a762b48f60f84736e861
kamiicode <luccacsg@gmail.com>
2026-04-25 18:32:50 -0300

    delete unused render.c and render.h

--- examples/shared/src/render.c
+++
@@ -1,8 +0,0 @@
-#include "render.h"
-
-#include <SDL3/SDL.h>
-
-void render_element(yuime_context_t* ctx, yuime_node_t* element, void* data) {
-	SDL_SetRenderDrawColor((SDL_Renderer*)data, 255, 0, 0, 255);
-	SDL_RenderFillRect((SDL_Renderer*)data, (SDL_FRect*)&element->rect);
-}
--- examples/shared/src/render.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <yuime/yuime.h>
-
-void render_element(yuime_context_t* ctx, yuime_node_t* element, void* data);
