b3c816da2bc5894d30b41d9e4720e05742ed1dae
kamiicode <luccacsg@gmail.com>
2026-04-25 18:33:15 -0300

    remove unused header

--- examples/shared/src/renderers/sdl.c
+++ examples/shared/src/renderers/sdl.c
@@ -48,7 +48,6 @@
 // 	return YUIME_CHILDREN_ITERATE_RETURN_SIGNAL_PASS;
 // }
 
-#include <SDL3/SDL_main.h>
 #include <SDL3/SDL.h>
 // #include <SDL3_ttf/SDL_ttf.h>
 // #include <SDL3_image/SDL_image.h>
